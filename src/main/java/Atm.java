576583b2ab8894f617787bad1312bc1521d69906
Fazıl Demir <fazildemir20@gmail.com>
2023-04-26 22:54:56 +0300

    dnm

--- src/main/java/Atm.java
+++ src/main/java/Atm.java
@@ -1,6 +1,16 @@
 public class Atm {
     public static void main(String[] args) {
         System.out.println("Bu bir Atm ");
-        //version1
+        //version4
+        System.out.println("Java is easy");
+        //version5
+        System.out.println("Learn Java Earn Money");
+        //version6
+        System.out.println("Git ogrenmek cok kolay");
+        //version7
+        System.out.println("Java ogrenmek ne kadar da zevkliymisa");
+        //version8
+        System.out.println("deneme branchi kodlarini yazdi");
+        //version dnm
     }
 }
